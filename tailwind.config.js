ab90bb064bdb2f2f5ac1635c6f8909f1f8701d0c
HuajiMUR233 <huajimur@jvav.me>
2024-12-10 12:06:37 +0800

    the quick brown fox jumps over the lazy dog

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ["Noto Sans SC", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      'serif': ["Noto Serif SC", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
+    },
     extend: {},
   },
   plugins: [],
